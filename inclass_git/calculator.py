db356d55f199fc94739d3222290f417ee1d52944
xiaooodd <63233120+xiaooodd@users.noreply.github.com>
2021-04-17 14:30:47 +0800

    Update calculator.py

--- inclass_git/calculator.py
+++ inclass_git/calculator.py
@@ -5,10 +5,10 @@ num2 = int(input("please anter a number:"))
 res1 = num1 + num2
 res2 = num1 - num2
 res3 = num1 * num2
-res4 = num1 / num2
+res4 = float(num1) / float(num2)
 
 print("%d + %d = %d" %(num1,num2,res1))
 print("%d - %d = %d" %(num1,num2,res2))
 print("%d * %d = %d" %(num1,num2,res3))
-print("%d / %d = %d" %(num1,num2,res4))
+print("%d / %d = %f" %(num1,num2,res4))
 
