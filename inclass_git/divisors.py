035d7250ac719f1d138c477709a345b3c8cf197c
xiaooodd <63233120+xiaooodd@users.noreply.github.com>
2021-04-17 14:31:27 +0800

    Update divisors.py

--- inclass_git/divisors.py
+++ inclass_git/divisors.py
@@ -1,8 +1,12 @@
-num = int(input("please enter an integer number:"))
-root = math.ceil(math.sqrt(num))
+def divisors(n):
+    if n == 0: return [0]
+    if n == 1: return [1]
+    rlist = []
+    for i in range(1,n+1):
+        if n%i == 0:
+            rlist.append(i)
 
-divisor = []
-for i in range(1,root+1):
-    if num % i == 0:
-        divisor.append(i)
-print(divisor)
+    return rlist
+    
+n = int(input("please enter an interge number:"))
+print(divisors(n))
